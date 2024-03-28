4bbc0aca1b0557ff1e110c524e9f646fc9559dbc
wanglihuaya <wanglihuaya@outlook.com>
2024-03-28 23:50:13 +0800

    feat: add dark-viewTransition

---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,13 @@
+// https://vitepress.dev/guide/custom-theme
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
+import './style.css'
+
+export default {
+  extends: DefaultTheme,
+  Layout: Layout,
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+} satisfies Theme
